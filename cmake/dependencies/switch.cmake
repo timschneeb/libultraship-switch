5a92dbca9d8478a3a03a8681fb3e118fa421c797
Tim Schneeberger <tim.schneeberger@outlook.de>
2025-12-26 23:48:04 +0100

    switch: install spdlog

--- cmake/dependencies/switch.cmake
+++ cmake/dependencies/switch.cmake
@@ -1,6 +1,19 @@
 # ========= ImGui =========
 target_include_directories(ImGui PRIVATE ${DEVKITPRO}/portlibs/switch/include/)
 
+# ========= spdlog =========
+find_package(spdlog QUIET)
+if (NOT ${spdlog_FOUND})
+    FetchContent_Declare(
+            spdlog
+            GIT_REPOSITORY https://github.com/gabime/spdlog.git
+            GIT_TAG v1.14.1
+            OVERRIDE_FIND_PACKAGE
+    )
+    FetchContent_MakeAvailable(spdlog)
+endif()
+
+
 # ========= StormLib =========
 if(INCLUDE_MPQ_SUPPORT)
     target_compile_definitions(StormLib PRIVATE -D_POSIX_C_SOURCE=200809L)
